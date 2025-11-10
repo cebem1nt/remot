1b2c2d7690e192590295dbc2b43c2a3cc531aee8
cebem1nt <mineewarik@gmail.com>
2025-11-10 12:49:30 -0300

    Small improvements, now can get file and execute some commands simply

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,150 +1,106 @@
-#include <cstdint>
-#include <cstdio>
+#include <cstddef>
 #include <cstdlib>
 #include <cstring>
-#include <fcntl.h>
-#include <iostream>
-#include <regex>
+#include <stdexcept>
 #include <string>
-#include <sys/ioctl.h>
-#include <sys/poll.h>
-#include <sys/types.h>
 #include <tgbot/tgbot.h>
+#include <regex>
 #include <unistd.h>
 
 #include "bot.hpp"
 
-using namespace std;
 using namespace TgBot;
 
 // Unfortunately telegram can not parse ansii escape codes :-(
 // We have to remove them
-string remove_ansii(string content) 
+std::string remove_ansii(std::string content) 
 {
-    regex ansii_regex(R"(\x1B\[[0-?9;]*[mK]|(\[\?2004[hl]))");
+    std::regex ansii_regex(R"(\x1B\[[0-?9;]*[mK]|(\[\?2004[hl]))");
     return regex_replace(content, ansii_regex, "");
-} 
+}
 
 class Shell {
 public:
-    int master_fd = -1;
-    int slave_fd = -1;
-
-    pid_t master_pid = -1;
-    pid_t slave_pid = -1;
-
     bool is_active = false;
+    int  master_fd = -1,        
+         slave_fd  = -1;
 
     void init_session() 
     {
-        this->master_fd = posix_openpt(O_RDWR | O_NONBLOCK);
-        grantpt(master_fd);
-        unlockpt(master_fd);
 
-        char* slave = ptsname(master_fd);
-        this->slave_fd = open(slave, O_RDWR);
-
-        struct winsize ws = {0};
-        ws.ws_row = 80;
-        ws.ws_col = 20;
-
-        ioctl(this->slave_fd, TIOCSWINSZ, &ws);
-
-        this->is_active = true; 
     }
 
     void end_session() 
     {
-        if (is_active) {
-            if (this->slave_pid > 0) {
-                kill(this->slave_pid, SIGTERM);
-            }
-
-            if (this->master_pid > 0) {
-                kill(this->master_pid, SIGTERM);
-            }
-
-            close(this->master_fd);
-            close(this->slave_fd);
-            this->is_active = false;
-        }
+       
     }
 
-    void open_shell() {
-        if (!is_active) return;
-
-        pid_t pid = fork();
-        if (pid == 0) {
-            close(this->master_fd);
-
-            setsid();
-            ioctl(slave_fd, TIOCSCTTY, 0);
-
-            dup2(slave_fd, STDOUT_FILENO);
-            dup2(slave_fd, STDIN_FILENO);
-            dup2(slave_fd, STDERR_FILENO);
-
-            close(this->slave_fd);
-
-            const char* shell = getenv("SHELL");
-            setenv("TERM", "dumb", 1);
-
-            this->slave_pid = getpid();
-
-            if (shell == NULL) {
-                execlp("sh", "sh", NULL);
-            } else {
-                execlp(shell, shell, NULL);
-            }
-        }
-    }
-
-    void exec(std::string cmd) {
-        cout << "Exec in shell: " << cmd << endl;
-        cmd += '\n';
-
-        const char* c_cli = cmd.c_str();
-        write(this->master_fd, c_cli, strlen(c_cli));
-    }
-
-    std::string peek() {
-        char buf[4096];
-        int n;
-
-        string output;
-
-        while ((n = read(this->master_fd, buf, sizeof(buf))) > 0) {
-            output.append(buf, static_cast<size_t>(n));
-        }
-
-        return remove_ansii(output);
+    void exec(std::string cmd) 
+    {
+       
     }
 };
 
-void poll_update_msg(
-    Bot& bot, Shell& sh, int64_t chat_id, int64_t terminal_msg_id
-) 
+void shell_loop() 
 {
-    struct pollfd fds[1];
-    fds[0].fd = sh.master_fd;
-    fds[0].events = POLLIN;
+    
+}
 
-    pid_t pid = fork();
-    if (pid == 0) {
-        while (true) {
-            int poll_res = poll(fds, 1, -1);
-            if (poll_res == -1) {
-                perror("poll");
-                exit(127);
-            }
+/*
+ * Just execute a command and return its stdout + stderr
+ */
+std::string exec(const char* cli) 
+{
+    std::string out;
+    char buf[BUFSIZ];
 
-            if (fds[0].revents & POLLIN) {
-                bot.getApi().editMessageText(
-                    "```Terminal\n" + sh.peek() + "\n```", 
-                    chat_id, terminal_msg_id, "", "Markdown");
-            }
-        }
+    FILE* pipe = popen(cli, "r");
+
+    if (pipe == NULL) 
+        throw std::runtime_error("Could not popen");
+    
+    while (fgets(buf, BUFSIZ, pipe) != NULL) {
+        out += buf;
     }
+
+    pclose(pipe);
+    return out;
+}
+
+std::string expand_user(std::string path) 
+{
+    std::string out;
+    size_t pos = path.find("~/");
+
+    if (pos != 0)
+        return path;
+
+    out += getenv("HOME");
+    out += path.substr(pos + 1);
+    return out;
+}
+
+bool file_exists(std::string path) 
+{
+    path = expand_user(path);
+    int tmp_fd = open(path.c_str(), O_RDONLY); 
+
+    if (tmp_fd == -1) {
+        return false;
+    } else {
+        close(tmp_fd);
+        return true;
+    }
+}
+
+std::string get_args(std::string msg)
+{
+    size_t pos = msg.find(' ');
+    if (pos + 1 >= msg.length()) {
+        return "";
+    }
+
+    return msg.substr(pos + 1);
 }
 
 int main() 
@@ -156,54 +112,69 @@ int main()
         int64_t user_id = message->from->id;
         int64_t chat_id = message->chat->id;
 
-        string msg = message->text;
+        std::string msg = message->text;
+        std::cout << "User wrote: " << user_id << " "
+                  << "Name: " << message->from->username 
+                  << '\n' ; 
 
-        cout << "User wrote: " << user_id << " "
-             << "Name: " << message->from->username 
-             << endl; 
-
-        cout << "Message: " << msg << endl;
+        std::cout << "Message: " << msg << '\n';
         
         if (user_id != ADMIN_ID) {
             bot.getApi().sendMessage(chat_id, "Who the fuck are you?");
             return;
         }
 
-        if (msg.find("/shell") == 0) {
-            if (sh.is_active) {
-                bot.getApi().sendMessage(chat_id, "Allready running");
+        if (msg.find("/exec") == 0) {
+            auto out = exec(get_args(msg).c_str());
+            bot.getApi().sendMessage(chat_id, out);
+        }
+
+        else if (msg.find("/file") == 0) {
+            auto file_path = expand_user(get_args(msg));
+
+            std::cout << "Requested file: " << file_path << '\n';
+            if (!file_exists(file_path.c_str())) {
+                bot.getApi().sendMessage(chat_id, "File does not exists");
                 return;
             }
 
-            auto terminal_msg = bot.getApi().sendMessage(chat_id, "Initializing terminal...");
-
-            sh.init_session();
-            sh.open_shell();
-            poll_update_msg(bot, sh, chat_id, terminal_msg->messageId);
-        }
-        else if (msg.find("/end") == 0) {
-            // TODO for some reason this pid -> kill aproach doesnt work 
-            sh.end_session();
+            auto file = InputFile::fromFile(file_path, "file");
+            bot.getApi().sendDocument(chat_id, file);
         }
 
-        else if (sh.is_active) {
-            sh.exec(message->text);
-            bot.getApi().deleteMessage(chat_id, message->messageId);
-        } 
-        else {
-            bot.getApi().sendMessage(chat_id, "Shell is not running");
-        }
+        // if (msg.find("/shell") == 0) {
+        //     if (sh.is_active) {
+        //         bot.getApi().sendMessage(chat_id, "Allready running");
+        //         return;
+        //     }
+
+        //     auto terminal_msg = bot.getApi().sendMessage(chat_id, "Initializing terminal...");
+
+        //     sh.init_session();
+        //     sh.open_shell();
+        //     poll_update_msg(bot, sh, chat_id, terminal_msg->messageId);
+        // }
+        // else if (msg.find("/end") == 0) {
+        //     sh.end_session();
+        // }
+        // else if (sh.is_active) {
+        //     sh.exec(message->text);
+        //     bot.getApi().deleteMessage(chat_id, message->messageId);
+        // } 
+        // else {
+        //     bot.getApi().sendMessage(chat_id, "Shell is not running");
+        // }
     });
 
-    TgLongPoll longPoll(bot);
-    cout << "Bot name: " << bot.getApi().getMe()->username << endl;
+    TgLongPoll longPoll(bot, 10, 5);
+    std::cout << "Bot name: " << bot.getApi().getMe()->username << '\n';
 
     while (true) {
         try {
             longPoll.start();
         } 
         catch (TgException& e) {
-            cerr << "LongPoll error: " << e.what() << " — restarting in 1s\n";
+            std::cerr << "LongPoll error: " << e.what() << " — restarting in 1s\n";
             sleep(1);
             continue;
         }
